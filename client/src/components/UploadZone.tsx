849aeac7edfe2ded20ba914f8028ba1fcea8a52c
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-25 14:46:10 +0500

    Added Props in Uploadzone file and initialized index file which define types of props

---
+++ client/src/components/UploadZone.tsx
@@ -0,0 +1,13 @@
+import type { UploadZoneProps } from "../types"
+
+const UploadZone = ({label,file,onClear,onChange}: UploadZoneProps) => {
+  return (
+    <div className="relative group">
+      <div className= {`relative h-64 rounded-2xl border-2 border-dashed transition-all duration-300 flex flex-col items-center justify-center bg-white/2 p-6`}>
+
+      </div>
+    </div>
+  )
+}
+
+export default UploadZone
