4d9abdd7baaba75b5a45c82a4dce4e75704bd5b6
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-25 17:53:52 +0500

    Added multiple state hhooks for file andling and images etc

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
 import Title from "../components/Title"
+import UploadZone from "../components/UploadZone"
 
 
 const Generator = () => {
+  const [name,setName] = useState('');
+  const [productName,setProductName] = useState('');
+  const [productDescription,setProductDescription] = useState('');
+  const [aspectRatio,setAspectRatio] = useState('9:16');
+    const [productImage,setProductImage] = useState<File | null>(null);
+  const [modelImage,setModelImage] = useState<File | null>(null);
+  const [userPrompt,setUserPrompt] = useState('');
+  const [isGenerating,setIsGenerating] = useState(false);
+  const 
   return (
     <div className="min-h-screen text-white p-6 md:p-12 mt-28">
       <form className="max-w-4xl mx-auto mb-40">
@@ -9,12 +20,7 @@ const Generator = () => {
           <div className="flex gap-20 max-sm:flex-col items-start justify-between">
             {/* left col */}
             <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
-              <p>
-                Product Image
-              </p>
-              <p>
-                Person Image
-              </p>
+              <UploadZone label="Upload Product Image" file={} onClear={() => {}} onChange={(e) => {}}/>
             </div>
             {/* Right col */}
             <div>
