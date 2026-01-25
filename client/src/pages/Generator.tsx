36dbd51ba67ead29ac6721b3af6f9d85119f465a
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-25 19:20:20 +0500

    Added Resolution resize buttons and userprompt textarea

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import Title from "../components/Title"
 import UploadZone from "../components/UploadZone"
 import { p } from "framer-motion/client";
+import { RectangleHorizontalIcon, RectangleVerticalIcon } from "lucide-react";
 
 
 const Generator = () => {
@@ -38,7 +39,7 @@ const Generator = () => {
             </div>
             {/* Right col */}
             <div className="w-full">
-              <div className="mb-4">
+              <div className="mb-4 text-gray-300">
                 <label htmlFor="name" className="block text-sm mb-4">Project Name</label>
                 <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name Your Project" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all"/>
               </div>
@@ -49,6 +50,17 @@ const Generator = () => {
               <div className="mb-4 text-gray-300">
                 <label htmlFor="productDescription" className="block text-sm mb-4">Product Description <span className="text-xs text-violet-400">(optional)</span></label>
                 <textarea id="productDescription" rows={4} value={productDescription} onChange={(e) => setProductDescription(e.target.value)} placeholder="Please Enter The Description Of The Product" className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all"/>
+              </div>
+              <div className="mb-4 text-gray-300">
+                <label className="block text-sm mb-4">AspectRatio</label>
+                <div className="flex gap-3">
+                    <RectangleVerticalIcon onClick={() => setAspectRatio('9:16')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '9:16' ? "ring-violet-500/50 bg-white/10" : ""}`}/>
+                      <RectangleHorizontalIcon onClick={() => setAspectRatio('16:9')} className={`p-2.5 size-13 bg-white/6 rounded transition-all ring-2 ring-transparent cursor-pointer ${aspectRatio === '16:9' ? "ring-violet-500/50 bg-white/10" : ""}`}/>
+                </div>
+              </div>
+              <div className="mb-4 text-gray-300">
+                <label htmlFor="userPrompt" className="block text-sm mb-4">User Prompt <span className="text-xs text-violet-400">(optional)</span></label>
+                <textarea id="userPrompt" rows={4} value={userPrompt} onChange={(e) => setUserPrompt(e.target.value)} placeholder="Please Describe Spectra What Kind Of Narration You Want" className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none resize-none transition-all"/>
               </div>
             </div>
           </div>
