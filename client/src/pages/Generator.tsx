a842273036cc169728fdb364b6d37cb4da28f7f0
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-25 18:05:41 +0500

    Developed a function to handle file chnaage

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import Title from "../components/Title"
 import UploadZone from "../components/UploadZone"
 
@@ -12,7 +12,15 @@ const Generator = () => {
   const [modelImage,setModelImage] = useState<File | null>(null);
   const [userPrompt,setUserPrompt] = useState('');
   const [isGenerating,setIsGenerating] = useState(false);
-  const 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement> ,type : 'product' | 'model') => {
+    if (e.target.files && e.target.files[0]) {
+      if (type === 'product') {
+        setProductImage(e.target.files[0]);
+      } else {
+        setModelImage(e.target.files[0]);
+      }
+    }
+  }
   return (
     <div className="min-h-screen text-white p-6 md:p-12 mt-28">
       <form className="max-w-4xl mx-auto mb-40">
