5f0e79933bcc7ddd58c166137c644055628efac4
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-25 14:18:49 +0500

    First creating generator page Added title and props

--- client/src/pages/Generator.tsx
+++ client/src/pages/Generator.tsx
@@ -1,8 +1,14 @@
+import Title from "../components/Title"
 
 
 const Generator = () => {
   return (
-    <div>Generator</div>
+    <div className="min-h-screen text-white p-6 md:p-12 mt-28">
+      <form className="max-w-4xl mx-auto mb-40">
+          <Title heading="Generate Contextual Image" description="Upload An Image Of Person And Product Images To Generate Stunning UGC, Short-term Videos And Commercial Reels."/>
+
+      </form>
+    </div>
   )
 }
 
