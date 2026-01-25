246b304f4fbeb0f34a401f76cc1dcfd12aa146ef
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-25 20:52:54 +0500

    Resolved resolution ascpect ratio bug

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -9,7 +9,7 @@ const ProjectCard = ({gen,setGenerations,forCommunity = false} : {gen:Project,se
     <div key={gen.id} className='mb-4 break-inside-avoid'>
      <div className='bg-white/5 border border-white/10 rounded-xl overflow-hidden hover:border-white/20 transition group'>
      {/* preview */}
-     <div>
+     <div className={`${gen?.aspectRatio === '9:16' ? 'aspect-9/16' : 'aspect-video'} relative overflow-hiddden`}>
        {gen.generatedImage && (
         <img src={gen.generatedImage} alt={gen.productName} className={`absolute inset-0 w-full h-full object-cover transition duration-500 ${gen.generatedImage ? 'group-hover:opacity-0' : 'group-hover:scale-105'}`}/>
        )}
