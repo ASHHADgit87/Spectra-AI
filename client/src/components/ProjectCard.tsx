0a693697edfe9ff5af4e98ee7314d4bb94dc61d4
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-26 17:47:55 +0500

    Added two division in project card in which tey will float one for model and one for product

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import type { Project } from '../types'
 import { useNavigate } from 'react-router-dom'
+import { Loader2Icon } from 'lucide-react';
 
 const ProjectCard = ({gen,setGenerations,forCommunity = false} : {gen:Project,setGenerations:React.Dispatch<React.SetStateAction<Project[]>>,forCommunity?:boolean}) => {
     const navigate = useNavigate();
@@ -16,6 +17,23 @@ const ProjectCard = ({gen,setGenerations,forCommunity = false} : {gen:Project,se
        {gen.generatedVideo && (
         <video src={gen.generatedVideo} muted loop playsInline className="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition duration-500" onMouseEnter={(e)=>e.currentTarget.play()} onMouseLeave={(e)=>e.currentTarget.pause()}/>
        )}
+       {(!gen.generatedImage && !gen.generatedVideo) && (
+        <div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center bg-black/20">
+            <Loader2Icon className='size-7 animate-spin'/>
+        </div>
+       )}
+       <div className='absolute left-3 top-3 flex items-center gap-2'>
+        {gen.isGenerating && (
+            <span className='text-xs px-2 py-1 bg-yellow-600/30 rounded-full'>Generating...</span>
+        )}
+        {gen.isPublished && (
+            <span className='text-xs px-2 py-1 bg-green-600/30 rounded-full'>Published</span>
+        )}    
+       </div>
+       <div className='absolute right-3 bottom-3'>
+          <img src={gen.uploadedImages[0]} alt='product' className='w-16 h-16 object-cover rounded-full animate-float'/>
+          <img src={gen.uploadedImages[1]} alt='model' className='w-16 h-16 object-cover rounded-full animate-float -ml-8' style={{animationDelay: '3s'}}/>
+       </div>
      </div>
      {/* Details */}
      <div>
