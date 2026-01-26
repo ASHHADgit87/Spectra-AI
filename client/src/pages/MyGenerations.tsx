9308fd2269f5c4ef4fc5bfdd75ab25d52280596f
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-26 18:15:54 +0500

    Started working on my generation page

--- client/src/pages/MyGenerations.tsx
+++ client/src/pages/MyGenerations.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import type { Project } from "../types";
+import { dummyGenerations } from "../assets/assets";
+import { Loader2Icon } from "lucide-react";
 
 const MyGenerations = () => {
-  return (
-    <div>MyGenerations</div>
+  const [generations,setGenerations] = useState<Project[]>([]);
+    const [loading,setLoading] = useState(true);
+    const fetchMyGenerations = async () => {
+      setTimeout(() => {
+        setGenerations(dummyGenerations);
+        setLoading(false);
+      },3000);
+    }
+    useEffect(() => {
+      fetchMyGenerations();
+    },[])
+  return loading ? (
+    <div className="flex items-center justify-center min-h-screen ">
+       <Loader2Icon className="size-7 animate-spin text-indigo-400"/>
+    </div>
+  ):
+  (
+     <div className="min-h-screen text-white p-6 md:p-12 my-28">
+      <div className="max-w-6xl mx-auto">
+        <header className="mb-12">
+          <h1 className="text-3xl md:text-4xl font-semibold mb-4">My Generations</h1>
+          <p className="text-gray-400">View And Manage Your Spectra-AI Content</p>
+        </header>
+        <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
+          {generations.map((project) => (
+            <ProjectCard gen={project} setGenerations={setProjects} forCommunity={true} key={project.id}/>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
