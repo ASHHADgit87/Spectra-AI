780f19dd27149ed5128f67d22a27364146adb3a9
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-31 13:04:55 +0500

    Started working on plans page

--- client/src/pages/Plans.tsx
+++ client/src/pages/Plans.tsx
@@ -1,7 +1,11 @@
+import Pricing from "../components/Pricing"
 
 const Plans = () => {
   return (
-    <div>Plans</div>
+    <div className="max-sm:py-10 sm:pt-20">
+      <Pricing/>
+      <p>Create Astonishing Images With Spectra AI For Just 5 Credits Per Generation and Generate Unique And Engaging Content Videos For 10 Credits.</p>
+    </div>
   )
 }
 
