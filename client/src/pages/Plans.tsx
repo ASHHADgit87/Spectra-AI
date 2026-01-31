33dbdddd8da3e3515e459e1bfcdfb4b4d21685ae
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-01-31 13:07:49 +0500

    Completed plans page

--- client/src/pages/Plans.tsx
+++ client/src/pages/Plans.tsx
@@ -4,7 +4,7 @@ const Plans = () => {
   return (
     <div className="max-sm:py-10 sm:pt-20">
       <Pricing/>
-      <p>Create Astonishing Images With Spectra AI For Just 5 Credits Per Generation and Generate Unique And Engaging Content Videos For 10 Credits.</p>
+      <p className="text-center text-gray-400 max-w-md text-sm my-14 mx-auto px-12">Create Astonishing Images With Spectra AI For Just <span className="text-indigo-400 font-medium">5 Credits</span> Per Generation and Generate Unique And Engaging Content Videos For <span className="text-indigo-400 font-medium">10 Credits</span>.</p>
     </div>
   )
 }
