7aa90c7da059f8c1ec2c398700b96f90a1ddf89c
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-02-01 21:03:08 +0500

    Added middle ware and global interface

---
+++ server/middlewares/auth.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+export const protect = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {userId} = req.auth();
+    if(!userId){
+      return res.status(401).json({ message: "Unauthorized User" });
+    }
+    next();
+  } catch (error: any) {
+    console.log(error);
+    res.status(401).json({ message: error.code || error.message });
+  }
+};
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express'
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
+app.use(clerkMiddleware())
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
