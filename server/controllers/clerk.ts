d2a77d493b7a7df655b6227e581e8ad46950c03b
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-02-02 14:53:24 +0500

    Completed clerk controller

--- server/controllers/clerk.ts
+++ server/controllers/clerk.ts
@@ -59,7 +59,9 @@ const clerkWebhooks = async (req: Request, res: Response) => {
             default:
                 break;
         }
-    } catch (error) {
-        
+        res.json({message: "Webhook Received :" + type})
+    } catch (error: any) {
+        res.status(500).json({message: error.message})
     }
 }
+export default clerkWebhooks
