af4e33d0daa05ba2b73f5bbeee32b7d1311ad0d3
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-02-02 18:08:27 +0500

    Completed all project function

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -18,7 +18,16 @@ export const getUserCredits = async (req: Request, res: Response) => {
 };
 export const getAllProjects  = async (req: Request, res: Response) => {
   try {
-    
+    const {userId} = req.auth();
+    const projects = await prisma.project.findMany({
+      where: {
+        userId
+      },
+      orderBy: {
+        createdAt: 'desc'
+      }
+    })
+    res.json({projects});
   } catch (error: any) {
     Sentry.captureException(error);
     res.status(500).json({ message: error.code || error.message });
