05290041f4c27484b48db8bbf658b46e10672e8b
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-02-02 15:25:17 +0500

    Added APi HTTp for clerk

--- server/server.ts
+++ server/server.ts
@@ -2,15 +2,18 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from './controllers/clerk.js';
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.use(cors())
+app.post('/api/clerk', express.raw({ type: 'application/json' }),  clerkWebhooks);
 app.use(express.json());
 app.use(clerkMiddleware())
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
