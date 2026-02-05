1875362b5e935161921c35cece68b0f81c2dffca
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-02-05 14:12:18 +0500

    Completed project routes file

---
+++ server/routes/ProjectRoutes.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  createProject,
+  createVideo,
+  getAllPublishedProjects,
+  deleteProject,
+} from "../controllers/ProjectController.js";
+import { protect } from "../middlewares/auth.js";
+const projectRouter = express.Router();
+projectRouter.post("/create", protect, createProject);
+projectRouter.post("/video", protect, createVideo);
+projectRouter.get("/published", getAllPublishedProjects);
+projectRouter.delete("/:projectId", protect, deleteProject);
+
+export default projectRouter;
